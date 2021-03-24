e218593c6226d685843fffad2b89cd67d7dd185a
execska1 <execska@gmail.com>
2021-03-24 16:39:00 +0100

    Implement methods in RadioCharts

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,39 @@
 package com.codecool.database;
 
+import java.sql.*;
 
 public class RadioCharts {
-    //TODO: implement this class
+    private final String URL;
+    private final String USER;
+    private final String PASS;
+
+    public RadioCharts(String URL, String USER, String PASS) {
+        this.URL = URL;
+        this.USER = USER;
+        this.PASS = PASS;
+    }
+
+    public String getMostPlayedSong() {
+        return execute("SELECT song FROM music_broadcast " +
+                "GROUP BY song " +
+                "ORDER BY SUM(times_aired) DESC LIMIT 1;");
+    }
+
+    public String getMostActiveArtist() {
+        return execute("SELECT artist FROM music_broadcast " +
+                "GROUP BY artist " +
+                "ORDER BY COUNT(DISTINCT song) DESC LIMIT 1;");
+    }
+
+    private String execute(String SQL) {
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASS)) {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL);
+            resultSet.next();
+            return resultSet.getString(1);
+        } catch (SQLException e) {
+            return "";
+        }
+    }
+
 }
